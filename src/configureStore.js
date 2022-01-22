f8d21fde767bb54813e702c6443660067615ef0e
luqmanmusah <musahluqman@gmail.com>
2022-01-22 22:52:47 +0000

    Separate front end from back-end

--- src/configureStore.js
+++ src/configureStore.js
@@ -4,7 +4,7 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 
 const initialState = {
-  msg: { title: 'Hello', body: 'English Message' },
+  msg: { title: 'Akwaaba', body: 'Akan Greetings' },
 };
 
 const rootReducer = (state = initialState, action) => {
